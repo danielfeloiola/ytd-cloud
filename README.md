54852da8be5978cc79582fb46bfcba75e652340a
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-06-11 17:54:33 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# youtube data api
+
+A simple Python tool to check YouTube recomendations
+The app will perform a searcj on youtube and return the most common videos
+Requires an API key from google
+
+this program is not finished
---
+++ application.py
@@ -0,0 +1,187 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required
+
+
+videos = []
+channels = []
+playlists = []
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///ytdata.db")
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Show the main page"""
+
+    if request.method == "POST":
+
+        #import the search function
+        from test import youtube_search
+
+        # get search str
+        query = request.form.get("query")
+
+        # debug
+        #print("debug test 1")
+
+        # call yt search function
+        try:
+            youtube_search(query)
+        except HttpError as e:
+            print("An HTTP error %d occurred:" + str(e.resp.status) + str(e.content))
+
+        print(videos)
+
+        return render_template("results.html", videos=videos)
+
+    # GET request
+    elif request.method == "GET":
+
+        return render_template("index.html")
+
+
+
+######
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        #makes the username global
+        username = request.form.get("username")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+     # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+    # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+       # Ensure passwords match
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords don't match", 403)
+
+        # encrypt passwprd
+        hash_password = generate_password_hash(request.form.get("password"))
+
+        # try to add user to database
+        result = db.execute("INSERT INTO users (username, hash) VALUES (:username, :hash_password)",
+                          username = request.form.get("username"),
+                          hash_password = hash_password
+                          )
+        if not result:
+            return apology("username already exists", 403)
+
+        # Do the login
+        session["user_id"] = result
+        print(session)
+
+        # makes username global
+        username = request.form.get("username")
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # if the user is looking for the login page
+    else:
+        return render_template("register.html")
+
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
