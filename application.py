b9247653837d1ff3e70cccb4c45662b43a6745ac
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-10-03 14:47:24 -0300

    session test

--- application.py
+++ application.py
@@ -61,6 +61,7 @@ def after_request(response):
 #app.config["SESSION_FILE_DIR"] = mkdtemp()
 #app.config["SESSION_PERMANENT"] = False
 #app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = 'pleasechangethesecretkey' 
 Session(app)
 
 #session['max_results'] = 5
