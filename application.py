9e57b8bee689ae3f34965198a07d220c6d547d60
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-10-03 14:44:30 -0300

    session test

--- application.py
+++ application.py
@@ -58,9 +58,9 @@ def after_request(response):
 
 
 # Configura a session
-app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+#app.config["SESSION_FILE_DIR"] = mkdtemp()
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 #session['max_results'] = 5
