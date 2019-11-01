9728de079407c1117c155625ebe22d0609fde241
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-10-31 21:30:09 -0300

    key fix

--- application.py
+++ application.py
@@ -56,7 +56,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 
 # secret key
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("KEY")
 
 # teste da session
 app.config["SESSION_PERMANENT"] = False
