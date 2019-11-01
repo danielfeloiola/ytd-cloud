35399361cbd975ae011cfb4c1733f8bc92c71116
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-11-01 00:28:48 -0300

    correção de bug na numeração da profundidade

--- application.py
+++ application.py
@@ -346,7 +346,7 @@ def coletar():
                     numeros.append(row[7])
 
 
-        if '1' in numeros:
+        if '0' in numeros:
             with open(nome_nodes, 'r') as csvfile2:
                 reader2 = csv.reader(csvfile2, delimiter=',')
 
