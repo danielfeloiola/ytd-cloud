c0e824fd9b14a57f7d63e516ab4f4662971f6a34
Daniel Loiola <29199121+danielfeloiola@users.noreply.github.com>
2019-10-31 21:59:02 -0300

    ???

--- application.py
+++ application.py
@@ -1211,4 +1211,4 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    socketio.run(app, host="http://ytd-cloud.herokuapp.com/", port=int(os.environ.get("PORT", 5000)))
